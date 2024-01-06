26bb5dff2c198826b9ff623955ba2f3cd50e0044
Ng Yuen Ning <ngyuenning@u.nus.edu>
2024-01-06 11:02:42 +0800

    fix package lock

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,11 @@
+// src/App.test.tsx
+// Remove or use React in your test if needed
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
