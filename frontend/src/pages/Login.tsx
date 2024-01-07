601e48f414fc1b7b33b146f0e86f4221947d7360
Ng Yuen Ning <ngyuenning@u.nus.edu>
2024-01-08 01:10:09 +0800

    implement user segregation in login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IPortkeyProvider, MethodsBase } from "@portkey/provider-types";
 import detectProvider from "@portkey/detect-provider";
 
 function Login() {
   const [provider, setProvider] = useState<IPortkeyProvider | null>(null);
   const [userAccount, setUserAccount] = useState<string | null>(null);
+  const [forceUpdate, setForceUpdate] = useState<boolean>(false);
 
   const init = async () => {
     try {
       // Detect blockchain provider
       const detectedProvider = await detectProvider();
       console.log("Detected Provider:", detectedProvider);
-      setProvider(detectedProvider);
+      if (detectedProvider) {
+        setProvider(detectedProvider);
+      }
     } catch (error) {
       console.error(error, "Error detecting provider");
     }
@@ -31,26 +34,32 @@ function Login() {
       console.log("Accounts:", accounts);
   
       // Handle user authentication
-      if (accounts && accounts.length > 0) {
-        setUserAccount(accounts[0]); // Assume the first account is the user's
+      if (accounts && Object.keys(accounts).length > 0) {
+        // Iterate over the accounts and choose the first one
+        let firstAccount = null;
+        for (const chain in accounts) {
+          if (accounts[chain].length > 0) {
+            firstAccount = accounts[chain][0];
+            break;
+          }
+        }
+  
+        setUserAccount(firstAccount);
       }
     } catch (error) {
       console.error(error, "Error connecting to provider or requesting accounts");
     }
   };
   
-
-  useEffect(() => {
-    if (!provider) {
-      console.log("Initializing...");
-      init();
-    }
-  }, [provider]);
-  
   useEffect(() => {
     console.log("userAccount changed:", userAccount);
+    setForceUpdate(!forceUpdate);
   }, [userAccount]);
-  
+
+  useEffect(() => {
+    console.log("Initializing...");
+    init();
+  }, []); // Run only on mount
 
   if (!provider) return <>Provider not found.</>;
 
